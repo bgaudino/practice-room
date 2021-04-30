28d26361c5df75478c7efd3ebbbc7ee812563d51
Brian <bgaudino@gmail.com>
2021-04-30 15:47:57 -0500

    edited routes

--- scripts/play.js
+++ scripts/play.js
@@ -1,9 +1,9 @@
 async function play(arr, octave, display, location, method, tempo) {
 
     for (let i = 0; i < arr.length; i++) {
-      $(location)(display[i]);
+      $(location).html(display[i]);
       Synth.play(0, arr[i], octave[i], 2);
       display[i]+= ' '
       await sleep(tempo);
-    } $(location.trim())(display);
+    } $(location.trim()).html(display);
 }
